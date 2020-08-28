3e77c539fc8ba36b53f1a220c0878b8300ac7f5e
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2020-08-28 12:37:56 -0300

    Commit init

---
+++ app.js
@@ -0,0 +1,22 @@
+// dependancies
+const express = require("express"),
+    app = express(),
+    port = process.env.port || 8080,
+    { static } = require("express");
+app.use(static("public"));
+app.use(static("views"));
+
+// Routes
+// Index
+app.get("/", function (req, res) {
+    res.render("index.html");
+});
+
+app.get("/*", function (req, res) {
+    res.render("index.html");
+});
+
+// Listen on port
+app.listen(port, function () {
+    console.log("server running on " + port)
+});
