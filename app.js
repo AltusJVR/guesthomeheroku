7c87cb3e8c9cc9c303a08b403eef6e1e521529f8
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2020-08-28 13:37:47 -0300

    Updated app.js file included more routes

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const express = require("express"),
     ejs = require("ejs");
 app.use(static("public"));
 app.use(static("views"));
-app.use("view engine", "ejs")
-
-
-
+app.set("view engine", "ejs");
 
 // Routes
 // Index
 app.get("/", function (req, res) {
     res.render("index");
+});
+
+// About
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
 });
 
 app.get("*", function (req, res) {
