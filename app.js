a6b91811ba5a3159470866f635a909fd20fe12f5
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2020-08-28 13:33:58 -0300

    Updated to ejs

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 const express = require("express"),
     app = express(),
     port = process.env.PORT || 8080,
-    { static } = require("express");
+    { static } = require("express"),
+    ejs = require("ejs");
 app.use(static("public"));
 app.use(static("views"));
+app.use("view engine", "ejs")
+
+
+
 
 // Routes
 // Index
 app.get("/", function (req, res) {
-    res.render("index.html");
+    res.render("index");
 });
 
 app.get("*", function (req, res) {
